0c9c85e2750f54db83d6e1a17cae53a7f0bb4f45
4mskumar <adityakr4ms@gmail.com>
2026-02-03 21:29:58 +0530

    frontend deployment error, lennis error, node_MODULES REINSTALL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import react from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import '../src/index.css'
+import "../src/index.css";
 import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <>
+  <React.StrictMode>
     <App />
     <Toaster />
-    </>
+  </React.StrictMode>
 );
